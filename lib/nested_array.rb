0dd1d417f0e2c5162cdd9f0382d2972d8fc6ce5d
Francisco Andaur <francisco.andaur@icloud.com>
2020-06-27 17:34:02 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -20,19 +20,3 @@ def assembled_matrix
   storage_room = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
 
 end
-
-def sorted_matrix
-  sorted_ORGANIC_PRODUCE = ORGANIC_PRODUCE.sort
-  sorted_CONVENTIONAL_PRODUCE = CONVENTIONAL_PRODUCE.sort
-  sorted = [sorted_CONVENTIONAL_PRODUCE, sorted_ORGANIC_PRODUCE]
-  sorted
-end
-
-def matrix_lookup(matrix, row, column)
-  return matrix[row][column]
-end
-
-def matrix_update(matrix, row, column, new_value)
-  matrix[row][column] = new_value
-  matrix
-end
